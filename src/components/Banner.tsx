a763d12065e90d7aac3df7d948642f673eea8e55
David Lee <Dai180293@gmail.com>
2024-08-20 10:04:42 +0100

    added all content and styling to banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,8 @@
 export const Banner = () => {
-  return <><h1>Banner</h1></>;
+    return (
+        <div className="flex justify-center items-center gap-1.5 text-center text-black py-3 font-medium bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)] opacity-70">
+            <p className="hidden md:block">Introducing a completely redesigned interface —</p>
+            <p className="underline">Explore the demo</p>
+        </div>
+    );
 };
