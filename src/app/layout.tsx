2ddc144c6e5109a7a943b3c536e6c40622e8234d
David Lee <Dai180293@gmail.com>
2024-08-20 10:04:54 +0100

    added all content and styling to navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={clsx(dmSans.className, "antialiased bg-black text-white")}>
+            <body className={clsx(dmSans.className, "antialiased bg-black text-white overflow-x-hidden")}>
                 <SmoothScrolling>{children}</SmoothScrolling>
             </body>
         </html>
