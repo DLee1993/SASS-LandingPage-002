cdf8c5698f38ad1c21ae0c06d45b98feb3dabd16
David Lee <Dai180293@gmail.com>
2024-08-23 20:33:21 +0100

    Added all animations to call to action component

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import cog from "@/assets/images/helix2.png";
 import pieChart from "@/assets/images/emojistar.png";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const CallToAction = () => {
+    const ctaRef = useRef<HTMLElement>(null);
     const [input, setInput] = useState("");
 
     const resetInput = () =>
@@ -13,8 +15,15 @@ export const CallToAction = () => {
             setInput("");
         }, 2000);
 
+    const { scrollYProgress } = useScroll({
+        target: ctaRef,
+        offset: ["start end", "end end"],
+    });
+
+    const translateY = useTransform(scrollYProgress, [0, 1], [20, -20]);
+
     return (
-        <section className="py-28">
+        <section ref={ctaRef} className="py-28">
             <div className="container relative flex justify-center items-center flex-col gap-7">
                 <h2 className="text-5xl md:text-6xl text-center font-bold tracking-tighter">
                     Get instant access
@@ -41,20 +50,31 @@ export const CallToAction = () => {
                         Get access
                     </button>
                 </form>
-                <Image
-                    src={pieChart}
-                    alt="cursor icon"
-                    width={200}
-                    height={200}
-                    className="h-auto hidden md:block absolute md:-left-24 xl:left-[88px] -top-28"
-                />
-                <Image
-                    src={cog}
-                    alt="message icon"
-                    width={200}
-                    height={200}
-                    className="h-auto hidden md:block absolute md:-right-24 xl:right-[50px] top-[60px]"
-                />
+                <motion.div
+                    className="hidden md:block absolute md:-left-24 xl:left-[88px] -top-28"
+                    style={{ translateY }}
+                >
+                    <Image
+                        src={pieChart}
+                        alt="cursor icon"
+                        width={200}
+                        height={200}
+                        className="h-auto max-w-none"
+                    />
+                </motion.div>
+
+                <motion.div
+                    className="hidden md:block absolute md:-right-24 xl:right-[50px] top-[60px]"
+                    style={{ translateY }}
+                >
+                    <Image
+                        src={cog}
+                        alt="message icon"
+                        width={200}
+                        height={200}
+                        className="h-auto max-w-none"
+                    />
+                </motion.div>
             </div>
         </section>
     );
